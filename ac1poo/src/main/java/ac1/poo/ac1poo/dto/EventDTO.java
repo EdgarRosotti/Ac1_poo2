9d219a2bc68dd9f49957909f721a7f41f639fa30
EdgarRosotti <edgarrosotti@hotmail.com>
2021-03-29 14:10:50 -0300

    Continuação

--- ac1poo/src/main/java/ac1/poo/ac1poo/dto/EventDTO.java
+++ ac1poo/src/main/java/ac1/poo/ac1poo/dto/EventDTO.java
@@ -4,28 +4,63 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 import ac1.poo.ac1poo.entities.Event;
-import lombok.Data;
 
-@Data
+
 public class EventDTO {
     private Long id;
     private String nome;
     private String local;
-    private LocalDate dataFinal;
     private LocalDate dataInicial;
     private LocalTime horarioInicial;
-    private LocalTime horarioFinal;
-    private String email;
-    public EventDTO(Event evento){
-        this.nome = evento.getNome();
-        this.local = evento.getLocal();
-        this.dataFinal = evento.getDataFinal();
-        this.dataInicial = evento.getDataInicial();
-        this.horarioInicial = evento.getHorarioInicial();
-        this.horarioFinal = evento.getHorarioFinal();
-        this.email = evento.getEmail();
-    }
-	public EventDTO(Long id2, String nome2, String local2, LocalDate dataFinal2, LocalDate dataInicial2,
-			LocalTime horarioInicial2, LocalTime horarioFinal2, String email2) {
+    public EventDTO() {
 	}
+
+    public EventDTO(Long id, String nome, String local, LocalDate dataInicial, LocalTime horarioInicial ) {
+       this.id=id;
+       this.nome=nome;
+       this.local=local;
+       this.dataInicial=dataInicial;
+       this.horarioInicial=horarioInicial;
+	}
+
+	public EventDTO(Event e) {
+        this.id = e.getId();
+        this.nome=e.getNome();
+        this.local=e.getLocal();
+        this.dataInicial=e.getDataInicial();
+        this.horarioInicial=e.getHorarioInicial();
+	}
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getLocal() {
+        return local;
+    }
+    public void setLocal(String local) {
+        this.local = local;
+    }
+    public LocalDate getDataInicial() {
+        return dataInicial;
+    }
+    public void setDataInicial(LocalDate dataInicial) {
+        this.dataInicial = dataInicial;
+    }
+    public LocalTime getHorarioInicial() {
+        return horarioInicial;
+    }
+    public void setHorarioInicial(LocalTime horarioInicial) {
+        this.horarioInicial = horarioInicial;
+    }
+  
+
 }
